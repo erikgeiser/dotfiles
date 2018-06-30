b721325319199bbdc0f139625d6884d20e8ebc4e
Erik Geiser <erik.geiser@rwth-aachen.de>
2018-06-30 11:46:29 +0200

    Add initial dotfiles and setup script

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# dotfiles
+# Dotfiles
+
+Repository for dotfile management.
+
+## Setup
+
+To setup the dotfiles run:
+
+```bash
+./setup.sh
+```
+
+This script will backup the old files into a `backup` folder inside this repo and create symlinks from the dotfiles in this repo to the home directory, where the dot is automatically prepended. If the files in the home directory is already a symlink, it will be skipped.
+
+## Add new dotfiles
+
+To add new dotfiles just create a new file in the `dotfiles` directory (without the dot for convernience) and add the name (without the path) to the `RCFILES` array in setup.sh
+
+
+## TODO
+
+*  Add option to automatically symlink any files in the `dotfiles` directory
---
+++ setup.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+RCFILES=(
+  "gitconfig"
+  "gitignore_global"
+  "inputrc"
+  "npmrc"
+  "screenrc"
+  "shellrc_physics"
+  "zprofile"
+  "zshrc"
+)
+
+action() {
+  local BASE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+  local DOTFILE_DIR=$BASE/dotfiles
+  local BACKUP_DIR=$BASE/backup
+
+  symlink() {
+    local GREEN='\033[0;32m'
+    local YELLOW='\033[0;33m'
+    local CYAN='\033[0;36m'
+    local NC='\033[0m' # No Color
+
+    local RCFILE=$1
+    if [[ -L ~/.$RCFILE ]]; then
+      printf "${YELLOW}[!] ~/.$RCFILE is already a symlink, skipping${NC}\n"
+    else
+      printf "${GREEN}[*] Creating symlink for ~/.$RCFILE${NC}\n"
+      if [[ -f ~/.$RCFILE ]]; then
+        printf "$   ${CYAN}-> [i] Backing up old file to $BACKUP_DIR/$RCFILE${NC}\n"
+        mv ~/.$RCFILE $BACKUP_DIR/$RCFILE
+      fi
+      ln -s $DOTFILE_DIR/$RCFILE ~/.$RCFILE
+    fi
+  }
+
+
+  mkdir -p $BACKUP_DIR
+
+  for RCFILE in "${RCFILES[@]}"; do
+    symlink $RCFILE
+  done
+}
+
+action "$@"
